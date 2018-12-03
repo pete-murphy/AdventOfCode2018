93c2a70a75e3f1371581020a3c6557b5845a33ca
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-03 12:15:21 -0500

    Removed extraneous lines

--- 01/javascript/main.js
+++ 01/javascript/main.js
@@ -1,11 +1,9 @@
 "use strict"
-let i = 0
 
 const findRepeat = (xs, acc = 0, set = new Set([0])) => {
   const go = ([y, ...ys], acc, set) =>
     y === undefined
       ? findRepeat(xs, acc, set)
       : (y_ => (set.has(y_) ? y_ : go(ys, y_, set.add(y_))))(y + acc)
-
   return go(xs, acc, set)
 }
