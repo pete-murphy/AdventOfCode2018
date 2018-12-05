4b30441a561f45097572007f5fd13adedaf5f50b
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-05 10:37:31 -0500

    Cleaned up day 5

--- src/Day5/Part1.hs
+++ src/Day5/Part1.hs
@@ -1,36 +1,18 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections       #-}
-
 module Day5.Part1 where
 
-import           Control.Arrow
 import           Data.Char
-import           Data.Function
-import           Data.List
-import           Data.List.Split
-import           Data.Map        (Map)
-import qualified Data.Map        as M
-import           Data.Maybe
-import           Data.Monoid
-import           Data.Ord
-import           Data.Set        (Set)
-import qualified Data.Set        as S
-import           Text.Trifecta
 
 parse :: String -> String
-parse (x:xs) = go [x] xs
+parse = go []
   where
-    go [] (y:ys)
-      | otherwise = go (y : []) ys
+    go [] (y:ys) = go [y] ys
     go acc [] = reverse $ tail acc
-    go acc@(a:as) rest@(y:ys)
+    go acc@(a:as) (y:ys)
       | y `reactsWith` a = go as ys
       | otherwise = go (y : acc) ys
     reactsWith x y =
       (isUpper x && toLower x == y) || (isLower x && toUpper x == y)
 
--- | Have to subtract 1 for '\n'
 solve :: String -> Int
 solve = length . parse
 
