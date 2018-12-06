7218911c9a87928996c260f9f06a67edaa90c0d6
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-06 09:49:30 -0500

    Part 2 passing sample test(??)

--- src/Day6/Part2.hs
+++ src/Day6/Part2.hs
@@ -1,1 +1,96 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day6.Part2 where
+
+import           Control.Arrow
+import           Data.Function
+import           Data.List
+import           Data.List.Split
+import           Data.Map        (Map)
+import qualified Data.Map        as M
+import           Data.Maybe
+import           Data.Ord
+
+type Coord = (Int, Int)
+
+newtype Location = L
+  { getL :: Coord
+  } deriving (Eq, Ord, Show)
+
+dist (cx, cy) = uncurry (+) . (abs . subtract cx *** abs . subtract cy) . getL
+
+isInRegion :: Int -> [Location] -> Coord -> Bool
+isInRegion n ls (cx, cy) = sum (map dist ls) <= n
+  where
+    dist = uncurry (+) . (abs . subtract cx *** abs . subtract cy) . getL
+
+findLargest :: [Location] -> [Maybe Location] -> Int
+findLargest ls =
+  length .
+  last .
+  sortOn length .
+  group . sort . filter (isCandidate $ findBounds ls) . catMaybes
+
+findBounds :: [Location] -> (Coord, Coord)
+findBounds xs =
+  let xMin = fst $ getL $ minimum xs
+      yMin = snd $ minimumVal $ getL <$> xs
+      xMax = fst $ getL $ maximum xs
+      yMax = snd $ maximumVal $ getL <$> xs
+   in ((xMin, yMin), (xMax, yMax))
+
+genTerritory :: (Coord, Coord) -> [Coord]
+genTerritory ((xMin, yMin), (xMax, yMax)) = do
+  x <- [xMin .. xMax]
+  y <- [yMin .. yMax]
+  pure (x, y)
+
+isCandidate :: (Coord, Coord) -> Location -> Bool
+isCandidate ((xMin, yMin), (xMax, yMax)) (L (x, y)) =
+  x > xMin && x < xMax && y > yMin && y < yMax
+
+parseLine :: String -> Location
+parseLine = L . (head &&& last) . map read . splitOn ", "
+
+-- ***************
+solve :: String -> Int
+solve input =
+  length . filter (isInRegion 1000 allLocs) . genTerritory . findBounds $
+  allLocs
+  where
+    getLocs = map parseLine . lines
+    allLocs = getLocs input
+
+solve_ :: String -> Int
+solve_ input =
+  length . filter (isInRegion 30 allLocs) . genTerritory . findBounds $ allLocs
+  where
+    getLocs = map parseLine . lines
+    allLocs = getLocs input
+
+main_ :: IO ()
+main_ = do
+  text <- readFile "sample.txt"
+  putStrLn $ show $ solve_ text
+
+-- ***************
+minimumValBy :: (b -> b -> Ordering) -> [(a, b)] -> (a, b)
+minimumValBy c = minimumBy (c `on` snd)
+
+maximumValBy :: (b -> b -> Ordering) -> [(a, b)] -> (a, b)
+maximumValBy c = maximumBy (c `on` snd)
+
+minimumVal :: Ord b => [(a, b)] -> (a, b)
+minimumVal = minimumValBy compare
+
+maximumVal :: Ord b => [(a, b)] -> (a, b)
+maximumVal = maximumValBy compare
+
+freqs :: Ord a => [a] -> Map a Int
+freqs = M.fromListWith (+) . map (, 1)
+
+-- ***************
+main :: IO ()
+main = do
+  text <- readFile "input.txt"
+  putStrLn $ show $ solve text
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -1,8 +1,7 @@
 module Day6Spec where
 
 import qualified Day6.Part1 as P1
-
--- import qualified Day6.Part2 as P2
+import qualified Day6.Part2 as P2
 import           Test.Hspec
 
 main :: IO ()
@@ -16,7 +15,7 @@ spec = do
       P1.solve sampleInput `shouldBe` 17
 --    it "sample input 2" $ do P1.solve "aabAAB\n" `shouldBe` 6
 --    it "sample input 3" $ do P1.solve "dabAcCaCBAcCcaDA\n" `shouldBe` 10
---  describe "Part2" $ do
---    it "actual input" $ do
---      input <- readFile "src/Day5/input.txt"
---      P2.solve input `shouldBe` 6188
+  describe "Part2" $ do
+    it "sample input" $ do
+      input <- readFile "src/Day6/sample.txt"
+      P2.solve_ input `shouldBe` 16
