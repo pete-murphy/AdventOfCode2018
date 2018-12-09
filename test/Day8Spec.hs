b83633e098cfca5fa876a6e730a29ed7554a18a6
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-09 17:08:56 -0500

    Day 8, part 1 clean up

--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -2,7 +2,6 @@ module Day8Spec where
 
 import qualified Day8.Part1 as P1
 import qualified Day8.Part2 as P2
-import qualified Day8Ignore as P_
 import           Test.Hspec
 
 main :: IO ()
@@ -10,10 +9,6 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Part1 (not mine)" $ do
-    it "actual input" $ do
-      sampleInput <- readFile "src/Day8/input.txt"
-      (P_.solve . P_.parse) sampleInput `shouldBe` 46096
   describe "Part1 (mine)" $ do
     it "actual input" $ do
       sampleInput <- readFile "src/Day8/input.txt"
