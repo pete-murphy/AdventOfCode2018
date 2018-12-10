9845c11c6c1dfcd60a99afbe1f11878c1af67ea3
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-10 13:42:15 -0500

    Day 10, part 1: need to render a grid 🤔

--- src/Day10/Part1.hs
+++ src/Day10/Part1.hs
@@ -1,27 +1,61 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day10.Part1 where
 
 import           Control.Arrow
+import           Control.Monad.State
+import           Data.Array
+import           Data.Bifunctor
 import           Data.Char
+import           Data.Function
+import           Data.List
 import           Data.List.Split
+import           Data.Ord
 import           Linear.V2
 
-type Pos = V2 Int
+type Coord = (Int, Int)
 
-type Vel = V2 Int
+type Pos = Coord
+
+type Vel = Coord
 
 parse :: String -> [(Pos, Vel)]
 parse = map parseLine . lines
   where
     parseLine = format . map read . filter (any isDigit) . splitOneOf "<,>"
-    format (px:py:vx:vy:_) = (V2 px py, V2 vx vy)
+    format (px:py:vx:vy:_) = ((px, py), (vx, vy))
 
 run :: [(Pos, Vel)] -> [(Pos, Vel)]
-run = map (\(p, v) -> (p + v, v))
+run = map ((uncurry (+) *** uncurry (+)) &&& snd)
 
-run' :: [(Pos, Vel)] -> [(Pos, Vel)]
-run' = map (uncurry (+) &&& snd)
+findBounds :: [(Pos, Vel)] -> (Pos, Pos)
+findBounds cs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
+  where
+    ps = map fst cs
+    xs = map fst ps
+    ys = map snd ps
+
+area :: (Pos, Pos) -> Int
+area ((x, y), (x', y')) = (x' - x) * (y' - y)
+
+preRun lights =
+  until
+    ((\(old, new) -> areaBounds new > areaBounds old) . (id &&& run))
+    run
+    lights
+  where
+    areaBounds = area . findBounds
+
+render :: [(Pos, Vel)] -> Array Coord String
+render lts =
+  array ((0, 0), (w, h)) $
+  map (, "X") $ map (bimap (subtract x) (subtract y) . fst) $ preRun lts
+  where
+    ((x, y), (x', y')) = findBounds lts
+    w = x' - x
+    h = y' - y
 
 main :: IO ()
 main = do
   text <- readFile "src/Day10/sample.txt"
-  mapM_ (putStrLn . show) $ (run &&& run') $ parse text
+  mapM_ (putStrLn . show) $ parse text
