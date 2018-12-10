e1ce274a78063fac0f83093ec85d02fba0f6edb8
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-10 17:34:57 -0500

    Day 10, part 1: grid rendered, now to render on each tick

--- src/Day10/Part1.hs
+++ src/Day10/Part1.hs
@@ -11,6 +11,7 @@ import           Data.Function
 import           Data.List
 import           Data.List.Split
 import           Data.Ord
+import           Debug.Trace
 import           Linear.V2
 
 type Coord = (Int, Int)
@@ -26,7 +27,7 @@ parse = map parseLine . lines
     format (px:py:vx:vy:_) = ((px, py), (vx, vy))
 
 run :: [(Pos, Vel)] -> [(Pos, Vel)]
-run = map ((uncurry (+) *** uncurry (+)) &&& snd)
+run = map (\((px, py), (vx, vy)) -> ((px + vx, py + vy), (vx, vy)))
 
 findBounds :: [(Pos, Vel)] -> (Pos, Pos)
 findBounds cs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
@@ -35,22 +36,42 @@ findBounds cs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
     xs = map fst ps
     ys = map snd ps
 
-area :: (Pos, Pos) -> Int
-area ((x, y), (x', y')) = (x' - x) * (y' - y)
-
-preRun lights =
-  until
-    ((\(old, new) -> areaBounds new > areaBounds old) . (id &&& run))
-    run
-    lights
+--  area :: (Pos, Pos) -> Int
+--  area ((x, y), (x', y')) = (x' - x) * (y' - y)
+--
+--  preRun :: [(Pos, Vel)] -> [(Pos, Vel)]
+--  preRun lights =
+--    until
+--      ((\(old, new) -> areaBounds new > areaBounds old) . (id &&& run))
+--      run
+--      lights
+--    where
+--      areaBounds = area . findBounds
+--
+--  render :: [(Pos, Vel)] -> Array Coord String
+--  render lts =
+--    accumArray (++) "" ((0, 0), (w, h)) $
+--    map (, "X") $ map (bimap (subtract x) (subtract y) . fst) $ lts
+--    where
+--      ((x, y), (x', y')) = findBounds lts
+--      w = x' - x
+--      h = y' - y
+render' :: [(Pos, Vel)] -> [String]
+render' lts =
+  trace (show (h, w)) $
+  map
+    (concat .
+     map
+       (\(a, b) ->
+          if (a, b) `elem` pts
+            then "#"
+            else ".")) $
+  groupBy ((\x y -> snd x == snd y)) $ do
+    b <- [y .. (y')]
+    a <- [x .. (x')]
+    pure $ (a, b)
   where
-    areaBounds = area . findBounds
-
-render :: [(Pos, Vel)] -> Array Coord String
-render lts =
-  array ((0, 0), (w, h)) $
-  map (, "X") $ map (bimap (subtract x) (subtract y) . fst) $ preRun lts
-  where
+    pts = map fst $ run $ run $ run lts
     ((x, y), (x', y')) = findBounds lts
     w = x' - x
     h = y' - y
@@ -58,4 +79,4 @@ render lts =
 main :: IO ()
 main = do
   text <- readFile "src/Day10/sample.txt"
-  mapM_ (putStrLn . show) $ parse text
+  mapM_ (putStrLn . show) $ render' $ parse text
