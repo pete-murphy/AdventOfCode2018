33683e7a4aff375bbf1d9ddcfb0e412dbaf2c684
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-09 19:17:38 -0500

    Finished Day 8

--- src/Day8/Part2.hs
+++ src/Day8/Part2.hs
@@ -1,12 +1,38 @@
 module Day8.Part2 where
 
-import           Data.Tree
+import           Debug.Trace
 
-solve :: Tree Int -> Int
-solve = undefined
+data Node = Node
+  { metadata :: [Int]
+  , children :: [Node]
+  } deriving (Eq, Ord, Show)
 
-parse :: String -> Tree Int
-parse = undefined
+mkTree :: [Int] -> ([Int], Node)
+mkTree (n:m:xs) = (xs'', Node ms cs)
+  where
+    (xs', cs) = mkChildren n (xs, [])
+    (ms, xs'') = splitAt m xs'
+    mkChildren :: Int -> ([Int], [Node]) -> ([Int], [Node])
+    mkChildren n' (ys, acc)
+      | n' == 0 = (ys, reverse acc)
+      | otherwise = mkChildren (n' - 1) (ys', (c : acc))
+      where
+        (ys', c) = mkTree ys
+
+parse :: String -> [Int]
+parse = map read . words
+
+solve :: [Int] -> Int
+solve = foldTree . snd . mkTree
+
+foldTree :: Node -> Int
+foldTree (Node ms cs)
+  | cs == [] = sum ms
+  | otherwise = sum $ foldTree <$> map ((!!) cs') ((subtract 1) <$> ms)
+  where
+    cs' = cs ++ repeat empty
+    empty :: Node
+    empty = Node [] []
 
 main :: IO ()
 main = do
--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -9,7 +9,11 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Part1 (mine)" $ do
+  describe "Part1" $ do
     it "actual input" $ do
       sampleInput <- readFile "src/Day8/input.txt"
       (P1.solve . P1.parse) sampleInput `shouldBe` 46096
+  describe "Part2" $ do
+    it "sample input" $ do
+      sampleInput <- readFile "src/Day8/sample.txt"
+      (P2.solve . P1.parse) sampleInput `shouldBe` 66
