413c109cfd922aa6214a288a8fbd6572e3c6dfa7
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-10 19:49:34 -0500

    Finished day 10
    Yikes, dirtiest day yet. Not sure why `until'` works when `preRun` doesn't 🤷‍♂️.

--- src/Day10/Part2.hs
+++ src/Day10/Part2.hs
@@ -1,1 +1,116 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day10.Part2 where
+
+import           Control.Arrow
+import           Control.Monad.State
+import           Data.Array
+import           Data.Bifunctor
+import           Data.Char
+import           Data.Function
+import           Data.List
+import           Data.List.Split
+import           Data.Ord
+import           Debug.Trace
+import           Linear.V2
+
+type Coord = (Int, Int)
+
+type Pos = Coord
+
+type Vel = Coord
+
+parse :: String -> [(Pos, Vel)]
+parse = map parseLine . lines
+  where
+    parseLine = format . map read . filter (any isDigit) . splitOneOf "<,>"
+    format (px:py:vx:vy:_) = ((px, py), (vx, vy))
+
+run :: [(Pos, Vel)] -> [(Pos, Vel)]
+run = map (\((px, py), (vx, vy)) -> ((px + vx, py + vy), (vx, vy)))
+
+findBounds :: [(Pos, Vel)] -> (Pos, Pos)
+findBounds cs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
+  where
+    ps = map fst cs
+    xs = map fst ps
+    ys = map snd ps
+
+area :: (Pos, Pos) -> Int
+area ((x, y), (x', y')) = (x' - x) * (y' - y)
+
+preRun :: [(Pos, Vel)] -> String
+preRun lights =
+  until'
+    0
+    ((\(old, new) -> areaBounds new > areaBounds old) .
+     (preRun' 2 &&& preRun' 3))
+    run
+    lights
+  where
+    areaBounds = area . findBounds
+
+until' :: Int -> (a -> Bool) -> (a -> a) -> a -> String
+until' n p f = go n
+  where
+    go n x
+      | p x = show n
+      | otherwise = go (n + 1) (f x)
+
+preRun_ :: Int -> [(Pos, Vel)] -> String
+preRun_ n lights
+  | ((\(old, new) -> areaBounds new > areaBounds old) .
+     (preRun' 10 &&& preRun' 11) $
+     preRun' n lights) = show $ n + 10
+  | otherwise = preRun_ (n + 1) lights
+  where
+    areaBounds = area . findBounds
+
+preRun' :: Int -> [(Pos, Vel)] -> [(Pos, Vel)]
+preRun' 0 xs = xs
+preRun' n xs = preRun' (n - 1) (run xs)
+
+findBounds'' :: [Pos] -> (Pos, Pos)
+findBounds'' ps = ((minimum xs, minimum ys), (maximum xs, maximum ys))
+  where
+    xs = map fst ps
+    ys = map snd ps
+
+render'' :: [Pos] -> [String]
+render'' pts =
+  trace (show (h, w)) $
+  map
+    (concat .
+     map
+       (\(a, b) ->
+          if (a, b) `elem` pts
+            then "#"
+            else ".")) $
+  groupBy ((\x y -> snd x == snd y)) $ do
+    b <- [y .. (y')]
+    a <- [x .. (x')]
+    pure $ (a, b)
+  where
+    ((x, y), (x', y')) = findBounds'' pts
+    w = x' - x
+    h = y' - y
+
+run' :: Int -> StateT [(Pos, Vel)] IO [Pos]
+run' n = do
+  replicateM n $ do
+    xs <- get
+    let xs' = map (\((p, p'), vs@(v, v')) -> ((p + v, p' + v'), vs)) xs
+    lift $ mapM_ (putStrLn . show) $ render'' (map fst xs')
+    put xs'
+  xs'' <- get
+  pure $ map fst xs''
+
+main :: IO ()
+main =
+  readFile "src/Day10/input.txt" >>= \text ->
+    putStrLn (preRun_ 0 $ parse text) >> (putStrLn "Done")
+
+main' :: IO ()
+main' =
+  readFile "src/Day10/input.txt" >>= \text ->
+    putStrLn (preRun $ parse text) >> (putStrLn "Done")
