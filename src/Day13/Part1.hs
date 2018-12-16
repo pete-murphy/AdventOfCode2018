ca9fd85d481b3b461a64bd98ba0d21b02fb77962
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-15 22:12:36 -0500

    D13P1, need to keep track of all updates to next CartArray

--- src/Day13/Part1.hs
+++ src/Day13/Part1.hs
@@ -3,6 +3,8 @@ module Day13.Part1 where
 import           Data.Array
 import           Data.Char
 import           Data.List
+import           Data.List.Split
+import           Debug.Trace
 
 data CartDirection
   = Up
@@ -29,11 +31,17 @@ data Track
   | I
   deriving (Eq)
 
+instance Show Cart where
+  show (Cart Up _) = "^"
+  show (Cart Dn _) = "v"
+  show (Cart Rt _) = ">"
+  show (Cart Lt _) = "<"
+
 instance Show Track where
   show H  = "-"
   show V  = "|"
-  show CF = "\\"
-  show CB = "/"
+  show CB = "\\"
+  show CF = "/"
   show I  = "+"
 
 data Cart = Cart
@@ -46,55 +54,123 @@ getNextTurn L = S
 getNextTurn S = R
 getNextTurn R = L
 
--- | For sample 1 only
+-- For sample 1 only
 _parse1 :: String -> [String]
 _parse1 =
   map (intersperse '\n') . filter (not . all isSpace) . transpose . lines
 
-type TrackArray = Array (Int, Int) (Track)
+type TrackArray = Array (Int, Int) (Maybe Track)
 
 type CartArray = Array (Int, Int) (Maybe Cart)
 
 type Collision = (Int, Int)
 
-tick :: TrackArray -> CartArray -> Either Collision CartArray
-tick ta ca = tick' (assocs ca)
+tick :: TrackArray -> CartArray -> Collision
+tick ta ca = trace (showCarts ca) tick' (assocs ca)
   where
-    tick' :: [((Int, Int), (Maybe Cart))] -> Either Collision CartArray
+    tick' :: [((Int, Int), (Maybe Cart))] -> Collision
     tick' (((x, y), Just (Cart Up t)):xs) =
-      case (ta ! (x - 1, y), ca ! (x - 1, y)) of
-        (_, Just _) -> Left (x - 1, y)
-        (I, _) -> Right $ ca // [((x - 1, y), next)]
-          where next =
+      case (ta ! nextCell, ca ! nextCell) of
+        (_, Just _) -> nextCell
+        (Just I, _) -> tick ta $ ca // [((x, y), Nothing), (nextCell, nextTurn)]
+          where nextTurn =
                   case getNextTurn t of
                     S -> Just $ Cart Up S
                     L -> Just $ Cart Lt L
                     R -> Just $ Cart Rt R
-        (CB, _) -> Right $ ca // [((x - 1, y), Just (Cart Rt t))]
+        (Just CB, _) -> tick ta $ ca // [(nextCell, Just (Cart Lt t))]
+        (Just CF, _) -> tick ta $ ca // [(nextCell, Just (Cart Rt t))]
+        (Just V, _) -> tick ta $ ca // [(nextCell, Just (Cart Up t))]
+        _ -> error $ "Something went wrong with up-facing cart" ++ show nextCell
+      where
+        nextCell = (x - 1, y)
+    tick' (((x, y), Just (Cart Dn t)):xs) =
+      trace (showTrack ta) $
+      case (ta ! nextCell, ca ! nextCell) of
+        (_, Just _) -> nextCell
+        (Just I, _) -> tick ta $ ca // [((x, y), Nothing), (nextCell, nextTurn)]
+          where nextTurn =
+                  case getNextTurn t of
+                    S -> Just $ Cart Dn S
+                    L -> Just $ Cart Rt L
+                    R -> Just $ Cart Lt R
+        (Just CB, _) -> tick ta $ ca // [(nextCell, Just (Cart Rt t))]
+        (Just CF, _) -> tick ta $ ca // [(nextCell, Just (Cart Lt t))]
+        (Just V, _) -> tick ta $ ca // [(nextCell, Just (Cart Dn t))]
+        _ ->
+          error $ "Something went wrong with down-facing cart" ++ show nextCell
+      where
+        nextCell = (x + 1, y)
+    tick' (((x, y), Just (Cart Rt t)):xs) =
+      case (ta ! nextCell, ca ! nextCell) of
+        (_, Just _) -> nextCell
+        (Just I, _) -> tick ta $ ca // [((x, y), Nothing), (nextCell, nextTurn)]
+          where nextTurn =
+                  case getNextTurn t of
+                    S -> Just $ Cart Rt S
+                    L -> Just $ Cart Up L
+                    R -> Just $ Cart Dn R
+        (Just CB, _) -> tick ta $ ca // [(nextCell, Just (Cart Dn t))]
+        (Just CF, _) -> tick ta $ ca // [(nextCell, Just (Cart Up t))]
+        (Just H, _) -> tick ta $ ca // [(nextCell, Just (Cart Rt t))]
+        _ ->
+          error $ "Something went wrong with right-facing cart" ++ show nextCell
+      where
+        nextCell = (x, y + 1)
+    tick' (((x, y), Just (Cart Lt t)):xs) =
+      case (ta ! nextCell, ca ! nextCell) of
+        (_, Just _) -> nextCell
+        (Just I, _) -> tick ta $ ca // [((x, y), Nothing), (nextCell, nextTurn)]
+          where nextTurn =
+                  case getNextTurn t of
+                    S -> Just $ Cart Lt S
+                    L -> Just $ Cart Dn L
+                    R -> Just $ Cart Up R
+        (Just CB, _) -> tick ta $ ca // [(nextCell, Just (Cart Up t))]
+        (Just CF, _) -> tick ta $ ca // [(nextCell, Just (Cart Dn t))]
+        (Just H, _) -> tick ta $ ca // [(nextCell, Just (Cart Lt t))]
+        _ ->
+          error $ "Something went wrong with left-facing cart" ++ show nextCell
+      where
+        nextCell = (x, y - 1)
+    tick' (((x, y), Nothing):xs) = tick' xs
+
+showTrack :: TrackArray -> String
+showTrack ta =
+  unlines . chunksOf (n + 1) . concatMap (maybe " " show . snd) . assocs $ ta
+  where
+    n = maximum . map (fst . fst) . assocs $ ta
+
+showCarts :: CartArray -> String
+showCarts ca =
+  unlines . chunksOf (n + 1) . concatMap (maybe " " show . snd) . assocs $ ca
+  where
+    n = maximum . map (fst . fst) . assocs $ ca
 
 parseTrack :: String -> TrackArray
 parseTrack s = listArray ((0, 0), (x, y)) . concatMap parseLine . lines $ s
+    -- Need to subtract 1 (array is 0-indexed)
   where
-    x = length $ head $ lines s
-    y = length $ lines s
-    parseLine :: String -> [Track]
+    x = subtract 1 $ length $ head $ lines s
+    y = subtract 1 $ length $ lines s
+    parseLine :: String -> [Maybe Track]
     parseLine []        = []
-    parseLine ('-':xs)  = H : parseLine xs
-    parseLine ('|':xs)  = V : parseLine xs
-    parseLine ('\\':xs) = CB : parseLine xs
-    parseLine ('/':xs)  = CF : parseLine xs
-    parseLine ('+':xs)  = I : parseLine xs
-    parseLine ('v':xs)  = V : parseLine xs
-    parseLine ('^':xs)  = V : parseLine xs
-    parseLine ('<':xs)  = H : parseLine xs
-    parseLine ('>':xs)  = H : parseLine xs
-    parseLine (_:xs)    = parseLine xs
+    parseLine ('-':xs)  = Just H : parseLine xs
+    parseLine ('|':xs)  = Just V : parseLine xs
+    parseLine ('\\':xs) = Just CB : parseLine xs
+    parseLine ('/':xs)  = Just CF : parseLine xs
+    parseLine ('+':xs)  = Just I : parseLine xs
+    parseLine ('v':xs)  = Just V : parseLine xs
+    parseLine ('^':xs)  = Just V : parseLine xs
+    parseLine ('<':xs)  = Just H : parseLine xs
+    parseLine ('>':xs)  = Just H : parseLine xs
+    parseLine (_:xs)    = Nothing : parseLine xs
 
 parseCarts :: String -> CartArray
 parseCarts s = listArray ((0, 0), (x, y)) . concatMap parseLine . lines $ s
   where
-    x = length $ head $ lines s
-    y = length $ lines s
+    x = subtract 1 $ length $ head $ lines s
+    y = subtract 1 $ length $ lines s
     parseLine :: String -> [Maybe Cart]
     parseLine []       = []
     parseLine ('v':xs) = Just (Cart Dn R) : parseLine xs
@@ -102,3 +178,14 @@ parseCarts s = listArray ((0, 0), (x, y)) . concatMap parseLine . lines $ s
     parseLine ('<':xs) = Just (Cart Lt R) : parseLine xs
     parseLine ('>':xs) = Just (Cart Rt R) : parseLine xs
     parseLine (_:xs)   = Nothing : parseLine xs
+
+solve :: String -> Collision
+solve t = tick ta ca
+  where
+    ta = parseTrack t
+    ca = parseCarts t
+
+main :: IO ()
+main = do
+  text <- readFile "src/Day13/sample/2.txt"
+  putStrLn $ show $ solve text
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -1,7 +1,8 @@
 module Day13Spec where
 
 import qualified Day13.Part1 as P1
-import qualified Day13.Part2 as P2
+
+-- import qualified Day13.Part2 as P2
 import           Test.Hspec
 
 main :: IO ()
@@ -10,9 +11,9 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Part1" $ do
-    it "small sample tick" $ do P1.tick ex1 `shouldBe` ex2
-    it "max PL of example grid (serial no. 18)" $ do
-      P1.solve 18 `shouldBe` (33, 45)
+    it "small sample tick" $ do
+      sample <- readFile "src/Day13/sample/2.txt"
+      P1.solve sample `shouldBe` (7, 3)
 --  describe "Part2" $ do
 --    it "max PL of example grid (serial no. 18)" $ do
 --      P2.solve 18 `shouldBe` ((90, 269), 16)
