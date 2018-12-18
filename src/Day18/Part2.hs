0d18b85bd2f7dbb0283ee2324e901f5b4eb4b4d6
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-18 14:39:08 -0500

    Finished D18P2, (test is off by one)

--- src/Day18/Part2.hs
+++ src/Day18/Part2.hs
@@ -1,7 +1,30 @@
 module Day18.Part2 where
 
-solve :: String -> (Int, Int)
-solve = undefined
+import           Control.Arrow
+import           Data.Set      (Set)
+import qualified Data.Set      as S
+import           Day18.Part1   hiding (main, solve)
+import           Util
+
+solve' :: String -> [Int]
+solve' t = resourceValue <$> iterate tick b
+  where
+    b = parse t
+
+type Start = Int
+
+accum :: Int -> [Int] -> (Start, [Int])
+accum m = go 1 S.empty
+  where
+    go n set (x:xs)
+      {- `m` is a magic number 🧙 -}
+      | x `S.member` set && n > m = (n, x : takeWhile (\y -> y /= x) xs)
+      | otherwise = go (n + 1) (S.insert x set) xs
+
+solve = valueAfter 1000000000 . accum 500 . solve'
+
+valueAfter :: Int -> (Int, [Int]) -> Int
+valueAfter x (n, ys) = (!!) ys $ (x - n) `mod` length ys
 
 main :: IO ()
 main = do
--- test/Day18Spec.hs
+++ test/Day18Spec.hs
@@ -13,3 +13,7 @@ spec = do
     it "sample" $ do
       sample <- readFile "src/Day18/sample.txt"
       P1.solve sample `shouldBe` 1147
+  describe "Part2" $ do
+    let ys = [1, 4, 5, 3, 2, 1, 5, 3, 2, 1]
+    it "accum" $ do P2.accum 0 ys `shouldBe` (6, [1, 5, 3, 2])
+    it "valueAfter" $ do P2.valueAfter 10 (P2.accum 0 ys) `shouldBe` (1)
