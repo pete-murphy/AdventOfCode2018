317c9abfe3f9cb1c36e8968e452cb00d69e87e1c
Peter Murphy <peter.francis.murphy@gmail.com>
2018-12-18 10:10:45 -0500

    D13P2, passing tests, refactored, still wrong

--- src/Day13/Part2.hs
+++ src/Day13/Part2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day13.Part2 where
 
 import           Data.Array
@@ -5,6 +7,8 @@ import           Data.Char
 import           Data.Function
 import           Data.List
 import           Data.List.Split
+import           Data.Map        (Map)
+import qualified Data.Map        as M
 import           Data.Maybe
 import           Data.Ord
 import           Debug.Trace
@@ -20,11 +24,19 @@ data CartTurn
   = L
   | S
   | R
-  deriving (Eq)
+  deriving (Eq, Ord, Show)
 
-data TurnDirection
-  = LTurn
-  | RTurn
+-- Not sure if this is a good idea
+instance Enum CartTurn where
+  succ L = S
+  succ S = R
+  succ R = L
+  toEnum 0 = L
+  toEnum 1 = S
+  toEnum 2 = R
+  fromEnum L = 0
+  fromEnum S = 1
+  fromEnum R = 2
 
 data Track
   = H
@@ -48,278 +60,125 @@ instance Show Track where
   show I  = "+"
 
 data Cart = Cart
-  { dir   :: CartDirection
-  , turns :: CartTurn
+  { heading  :: CartDirection
+  , lastTurn :: CartTurn
   } deriving (Eq)
 
-getNextTurn :: CartTurn -> CartTurn
-getNextTurn L = S
-getNextTurn S = R
-getNextTurn R = L
+newtype Coord = Coord
+  { unCoord :: (Int, Int)
+  } deriving (Eq, Show)
 
--- For sample 1 only
-_parse1 :: String -> [String]
-_parse1 =
-  map (intersperse '\n') . filter (not . all isSpace) . transpose . lines
+instance Ord Coord where
+  compare (Coord (x, y)) (Coord (x', y')) = compare (y, x) (y', x')
 
-type TrackArray = Array (Int, Int) (Maybe Track)
+type TrackMap = Map Coord Track
 
-type CartArray = Array (Int, Int) (Maybe Cart)
+type CartMap = Map Coord Cart
 
-type LastRemaining = (Int, Int)
+findLastCart :: Map Coord Cart -> Maybe Coord
+findLastCart cs
+  | M.size cs > 1 = Nothing
+  | otherwise = listToMaybe $ M.keys cs
 
-findDuplicateCoord :: [((Int, Int), a)] -> Maybe (Int, Int)
-findDuplicateCoord xs = findDuplicateCoord' xs'
+cartMove :: Coord -> Cart -> Coord
+cartMove (Coord (x, y)) (Cart h _) =
+  case h of
+    Up -> Coord (x, y - 1)
+    Dn -> Coord (x, y + 1)
+    Rt -> Coord (x + 1, y)
+    Lt -> Coord (x - 1, y)
+
+-- There must be a better way :<
+cartTurn :: Cart -> Track -> Cart
+cartTurn (Cart h t) track =
+  case track of
+    I ->
+      case (h, t) of
+        (Up, L) -> Cart Lt (succ t)
+        (Up, S) -> Cart Up (succ t)
+        (Up, R) -> Cart Rt (succ t)
+        (Rt, L) -> Cart Up (succ t)
+        (Rt, S) -> Cart Rt (succ t)
+        (Rt, R) -> Cart Dn (succ t)
+        (Dn, L) -> Cart Rt (succ t)
+        (Dn, S) -> Cart Dn (succ t)
+        (Dn, R) -> Cart Lt (succ t)
+        (Lt, L) -> Cart Dn (succ t)
+        (Lt, S) -> Cart Lt (succ t)
+        (Lt, R) -> Cart Up (succ t)
+    CB ->
+      case h {- CB is \ -}
+            of
+        Up -> Cart Lt t
+        Dn -> Cart Rt t
+        Lt -> Cart Up t
+        Rt -> Cart Dn t
+    CF ->
+      case h {- CF is / -}
+            of
+        Up -> Cart Rt t
+        Dn -> Cart Lt t
+        Lt -> Cart Dn t
+        Rt -> Cart Up t
+    _ -> Cart h t
+
+tick :: TrackMap -> CartMap -> Coord
+tick tmap cmap = traceShow (M.size cmap) tick' (M.assocs cmap) M.empty
   where
-    xs' = sort $ map fst xs
-    findDuplicateCoord' [] = Nothing
-    findDuplicateCoord' [x] = Nothing
-    findDuplicateCoord' (x:y:xs'') =
-      if x == y
-        then Just x
-        else findDuplicateCoord' (y : xs'')
+    tick' :: [(Coord, Cart)] -> Map Coord Cart -> Coord
+    tick' [] acc =
+      case findLastCart acc of
+        Nothing -> tick tmap acc
+        Just x  -> x
+    tick' ((coord, cart):xs) acc =
+      let nextCoord = cartMove coord cart
+          cart' = cartTurn cart (tmap M.! nextCoord)
+       in case acc M.!? nextCoord of
+            Nothing -> tick' xs $ M.insert nextCoord cart' acc
+            Just _  -> tick' xs $ M.delete nextCoord acc
 
-findLastCart :: CartArray -> Maybe (Int, Int)
-findLastCart ca = findLastCart' (catMaybes' $ assocs ca)
-  where
-    catMaybes' :: [(a, Maybe b)] -> [(a, b)]
-    catMaybes' []               = []
-    catMaybes' ((x, Just y):xs) = (x, y) : catMaybes' xs
-    catMaybes' (_:xs)           = catMaybes' xs
-    findLastCart' xs
-      | length xs > 1 = Nothing
-    findLastCart' [(coord, cart)] = traceShow cart $ Just coord
+withCoords :: [String] -> [(Coord, Char)]
+withCoords =
+  concat .
+  zipWith (\y -> map (\(x, c) -> (Coord (x, y), c))) [0 ..] . map (zip [0 ..])
 
-tick :: TrackArray -> CartArray -> LastRemaining
-tick ta ca =
-  tick'
-    []
-    (concat $
-     transpose $
-     chunksOf (length $ takeWhile (\((x, _), _) -> x == 0) $ assocs ca) $
-     assocs ca)
-  where
-    tick' ::
-         [((Int, Int), Maybe Cart)]
-      -> [((Int, Int), Maybe Cart)]
-      -> LastRemaining
-    tick' acc [] =
-      case findLastCart (ca // acc) of
-        Nothing -> tick ta (ca // acc)
-        Just c  -> c
-    tick' acc (((x, y), Just (Cart Up t)):xs) =
-      let nextCell = (x, y - 1)
-          thisCell = (x, y)
-       in if nextCell `lookup` acc /= Nothing &&
-             nextCell `lookup` acc /= Just Nothing
-            then tick' (acc ++ [(nextCell, Nothing), (thisCell, Nothing)]) xs
-            else case (ta ! nextCell, ca ! nextCell) of
-                   (_, Just _) -> tick' nextAcc xs
-                     where nextAcc =
-                             acc ++ [(thisCell, Nothing), (nextCell, Nothing)]
-                   (Just I, _) ->
-                     tick'
-                       (acc ++ [(thisCell, Nothing), (nextCell, nextTurn)])
-                       xs
-                     where nextTurn =
-                             case getNextTurn t of
-                               S -> Just $ Cart Up S
-                               L -> Just $ Cart Lt L
-                               R -> Just $ Cart Rt R
-                   (Just CB, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Lt t))])
-                       xs
-                   (Just CF, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Rt t))])
-                       xs
-                   (Just V, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Up t))])
-                       xs
-                   _ ->
-                     error $
-                     "Something went wrong with up-facing cart" ++ show nextCell
-    tick' acc (((x, y), Just (Cart Dn t)):xs) =
-      let nextCell = (x, y + 1)
-          thisCell = (x, y)
-       in if nextCell `lookup` acc /= Nothing &&
-             nextCell `lookup` acc /= Just Nothing
-            then tick' (acc ++ [(nextCell, Nothing), (thisCell, Nothing)]) xs
-            else case (ta ! nextCell, ca ! nextCell) of
-                   (_, Just _) ->
-                     tick' nextAcc $ filter (\(c, _) -> c /= nextCell) xs
-                     where nextAcc =
-                             acc ++ [(thisCell, Nothing), (nextCell, Nothing)]
-                   (Just I, _) ->
-                     tick'
-                       (acc ++ [(thisCell, Nothing), (nextCell, nextTurn)])
-                       xs
-                     where nextTurn =
-                             case getNextTurn t of
-                               S -> Just $ Cart Dn S
-                               L -> Just $ Cart Rt L
-                               R -> Just $ Cart Lt R
-                   (Just CB, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Rt t))])
-                       xs
-                   (Just CF, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Lt t))])
-                       xs
-                   (Just V, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Dn t))])
-                       xs
-                   (x, _) ->
-                     error $
-                     "Something went wrong with down-facing cart" ++
-                     show nextCell ++
-                     "\n" ++
-                     show thisCell ++
-                     "\n" ++ show x ++ "\n" ++ show (ta ! nextCell)
-    tick' acc (((x, y), Just (Cart Rt t)):xs) =
-      let nextCell = (x + 1, y)
-          thisCell = (x, y)
-       in if nextCell `lookup` acc /= Nothing &&
-             nextCell `lookup` acc /= Just Nothing
-            then tick' (acc ++ [(nextCell, Nothing), (thisCell, Nothing)]) xs
-            else case (ta ! nextCell, ca ! nextCell) of
-                   (_, Just _) -> tick' nextAcc $ tail xs
-                     where nextAcc =
-                             acc ++ [(thisCell, Nothing), (nextCell, Nothing)]
-                   (Just I, _) ->
-                     tick'
-                       (acc ++ [(thisCell, Nothing), (nextCell, nextTurn)])
-                       xs
-                     where nextTurn =
-                             case getNextTurn t of
-                               S -> Just $ Cart Rt S
-                               L -> Just $ Cart Up L
-                               R -> Just $ Cart Dn R
-                   (Just CB, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Dn t))])
-                       xs
-                   (Just CF, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Up t))])
-                       xs
-                   (Just H, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Rt t))])
-                       xs
-                   _ ->
-                     error $
-                     "Something went wrong with right-facing cart" ++
-                     show nextCell
-    tick' acc (((x, y), Just (Cart Lt t)):xs) =
-      let nextCell = (x - 1, y)
-          thisCell = (x, y)
-       in if nextCell `lookup` acc /= Nothing &&
-             nextCell `lookup` acc /= Just Nothing
-            then tick' (acc ++ [(nextCell, Nothing), (thisCell, Nothing)]) xs
-            else case (ta ! nextCell, ca ! nextCell) of
-                   (_, Just _) -> tick' nextAcc xs
-                     where nextAcc =
-                             acc ++ [(thisCell, Nothing), (nextCell, Nothing)]
-                   (Just I, _) ->
-                     tick'
-                       (acc ++ [(thisCell, Nothing), (nextCell, nextTurn)])
-                       xs
-                     where nextTurn =
-                             case getNextTurn t of
-                               S -> Just $ Cart Lt S
-                               L -> Just $ Cart Dn L
-                               R -> Just $ Cart Up R
-                   (Just CB, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Up t))])
-                       xs
-                   (Just CF, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Dn t))])
-                       xs
-                   (Just H, _) ->
-                     tick'
-                       (acc ++
-                        [(thisCell, Nothing), (nextCell, Just (Cart Lt t))])
-                       xs
-                   _ ->
-                     error $
-                     "Something went wrong with left-facing cart" ++
-                     show nextCell
-    tick' acc ((_, Nothing):xs) = tick' acc xs
+charToTrack :: Char -> Maybe Track
+charToTrack =
+  \case
+    '-' -> Just H
+    '|' -> Just V
+    '\\' -> Just CB
+    '/' -> Just CF
+    '+' -> Just I
+    'v' -> Just V
+    '^' -> Just V
+    '<' -> Just H
+    '>' -> Just H
+    _ -> Nothing
 
-parseTrack :: String -> TrackArray
-parseTrack s =
-  array ((0, 0), (x, y)) .
-  map (\((y, x), t) -> ((x, y), t)) .
-  assocs . listArray ((0, 0), (y, x)) . concatMap parseLine . lines $
-  s
-    -- Need to subtract 1 (array is 0-indexed)
-  where
-    x = subtract 1 $ length $ head $ lines s
-    y = subtract 1 $ length $ lines s
-    parseLine :: String -> [Maybe Track]
-    parseLine []        = []
-    parseLine ('-':xs)  = Just H : parseLine xs
-    parseLine ('|':xs)  = Just V : parseLine xs
-    parseLine ('\\':xs) = Just CB : parseLine xs
-    parseLine ('/':xs)  = Just CF : parseLine xs
-    parseLine ('+':xs)  = Just I : parseLine xs
-    parseLine ('v':xs)  = Just V : parseLine xs
-    parseLine ('^':xs)  = Just V : parseLine xs
-    parseLine ('<':xs)  = Just H : parseLine xs
-    parseLine ('>':xs)  = Just H : parseLine xs
-    parseLine (_:xs)    = Nothing : parseLine xs
+charToCart :: Char -> Maybe Cart
+charToCart =
+  \case
+    'v' -> Just $ Cart Dn L
+    '^' -> Just $ Cart Up L
+    '>' -> Just $ Cart Rt L
+    '<' -> Just $ Cart Lt L
+    _ -> Nothing
 
-parseCarts :: String -> CartArray
-parseCarts s =
-  array ((0, 0), (x, y)) .
-  map (\((y, x), c) -> ((x, y), c)) .
-  assocs . listArray ((0, 0), (y, x)) . concatMap parseLine . lines $
-  s
-  where
-    x = subtract 1 $ length $ head $ lines s
-    y = subtract 1 $ length $ lines s
-    parseLine :: String -> [Maybe Cart]
-    parseLine []       = []
-    parseLine ('v':xs) = Just (Cart Dn R) : parseLine xs
-    parseLine ('^':xs) = Just (Cart Up R) : parseLine xs
-    parseLine ('<':xs) = Just (Cart Lt R) : parseLine xs
-    parseLine ('>':xs) = Just (Cart Rt R) : parseLine xs
-    parseLine (_:xs)   = Nothing : parseLine xs
+parseTrack :: String -> TrackMap
+parseTrack =
+  M.fromList . catMaybes' . map (fmap charToTrack) . withCoords . lines
 
-showCartsAndTrack :: CartArray -> TrackArray -> String
-showCartsAndTrack ca ta =
-  unlines $
-  transpose $
-  chunksOf (n + 1) $
-  concatMap snd $
-  assocs
-    (array (bounds ta) (fmap showMaybe <$> (assocs ta)) //
-     (fmap showMaybe <$> (filter (not . (== Nothing) . snd) (assocs ca))))
-  where
-    showMaybe (Just x) = show x
-    showMaybe Nothing  = " "
-    n = maximum . map (snd . fst) . assocs $ ta
+parseCarts :: String -> CartMap
+parseCarts =
+  M.fromList . catMaybes' . map (fmap charToCart) . withCoords . lines
 
-solve :: String -> LastRemaining
-solve t = tick ta ca
+catMaybes' []                = []
+catMaybes' ((c, Just x):xs)  = (c, x) : catMaybes' xs
+catMaybes' ((c, Nothing):xs) = catMaybes' xs
+
+solve :: String -> (Int, Int)
+solve t = unCoord $ tick ta ca
   where
     ta = parseTrack t
     ca = parseCarts t
